71ce360bf05bf58850b281070a5aa7f2431a9786
yfyang <poplar1123@gmail.com>
2011-08-18 10:18:31 +0800

    删除原来的目录结构
    Signed-off-by: yfyang <poplar1123@gmail.com>

--- generate_tool/tool_ga/run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set /p netpath=����������ļ���ŵ�ַ��
-java -jar nut_genterate.jar %netpath%
-pause
--- generate_tool/tool_ga/run.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-java="$JAVA_HOME/bin/java"
-echo "Enter spring and mybatis's file's path:"
-read PATH
-exec $java -jar nut_genterate.jar $PATH
